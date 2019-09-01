3c237f7433cd71c2fcc84311efc9b4dfc068ecbb
Jerem <jeremy1.pidjot@epitech.eu>
2019-09-01 11:53:55 +0200

    configure: store & redux-dev_tools configured

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import "./styles/index.css";
+import App from '../src/containers/App'
+import { Provider } from 'react-redux';
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import configureStore from './store/configureStore';
 
+const initialState = {};
+const store = configureStore(initialState);
+const MOUNT_NODE = document.getElementById('app');
+
+const render = () => {
+    ReactDOM.render(
+        <Provider store={store}>
+            <App/>
+        </Provider>,
+        document.getElementById("root")
+    );
+};
+
+// if (module.hot) {
+//     // Hot reloadable React components and translation json files
+//     // modules.hot.accept does not accept dynamic dependencies,
+//     // have to be constants at compile-time
+//     module.hot.accept(['containers/App'], () => {
+//         ReactDOM.unmountComponentAtNode(MOUNT_NODE);
+//         render();
+//     });
+// }
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+render();
---
+++ src/store/configureStore.js
@@ -0,0 +1,16 @@
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createReducer from './reducers';
+
+export default function configureStore(initialState = {}) {
+    const middlewares = [thunk];
+    const enhancers = [applyMiddleware(...middlewares)];
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+    const store = createStore(createReducer, initialState, composeEnhancers(...enhancers));
+
+    store.asyncReducers = {};
+    return store;
+}
+
+
