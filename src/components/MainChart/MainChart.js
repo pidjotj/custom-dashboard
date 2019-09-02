e850446b2c8e3b22cbc18f4ee212a46ff15a1bdc
Jerem <jeremy1.pidjot@epitech.eu>
2019-09-02 17:44:36 +0200

    add: Carousel & enhance chart

--- src/components/MainChart/MainChart.js
+++ src/components/MainChart/MainChart.js
@@ -1,17 +1,40 @@
 import React from "react";
-import PropTypes from 'prop-types';
-import * as d3 from 'd3';
+import '../../styles/index.css'
 import { connect } from 'react-redux';
 import Chart from "react-apexcharts";
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, ButtonGroup, Button } from 'reactstrap';
+import {
+  ButtonGroup,
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
 import DateDropDown from "../DateDropDown";
 import VariableDropDown from "../VariableDropDown";
+
+const items = [
+  {
+    id: 0,
+    type: 'line',
+  },
+  {
+    id: 1,
+    type: 'bar'
+  }
+];
 
 class MainChart extends React.Component {
   constructor(props) {
     super(props);
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+    this.goToIndex = this.goToIndex.bind(this);
+    this.onExiting = this.onExiting.bind(this);
+    this.onExited = this.onExited.bind(this);
 
     this.state = {
+      activeIndex: 0,
       keyDateResult: null,
       fromResult: '',
       toResult: '',
@@ -29,8 +52,12 @@ class MainChart extends React.Component {
         },
         dataLabels: {
           style: {
-            colors: ['#F44336', '#E91E63', '#9C27B0']
+            colors: ['#FFFFFF', '#E91E63', '#9C27B0']
           }
+        },
+        colors: ["#FF1654", "#247BA0"],
+        tooltip: {
+          theme: 'dark',
         }
       },
       series: [
@@ -42,9 +69,37 @@ class MainChart extends React.Component {
         //   name: "series-2",
         //   data: [30, 40, 45, 50, 49, 60, 170, 391, 530, 340, 45, 50, 49, 60, 70, 91]
         // }
-      ]
+      ],
     };
   }
+
+  // CAROUSEL METHODS
+  onExiting() {
+    this.animating = true;
+  }
+
+  onExited() {
+    this.animating = false;
+  }
+
+  next() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  previous() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  goToIndex(newIndex) {
+    if (this.animating) return;
+    this.setState({ activeIndex: newIndex });
+  }
+
+  // END CAROUSEL METHODS
 
   receiveCallbackFrom(key) {
       this.setState({ keyDateResult: key });
@@ -80,6 +135,7 @@ class MainChart extends React.Component {
           let newTab = MainChart.parseTable(tempVariableData);
           let newSeries = JSON.parse(JSON.stringify(this.state.series));
           newSeries[0].data = newTab;
+          newSeries[0].name = test;
           this.setState({
             series: newSeries
           }, () => {
@@ -92,9 +148,29 @@ class MainChart extends React.Component {
 
   render() {
     const { metricsFile } = this.props;
-    const { keyDateResult } = this.state;
+    const { keyDateResult, activeIndex, options, series } = this.state;
+
+    const slides = items.map((item) => {
+      return (
+        <CarouselItem
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+          key={item.id}
+        >
+          <div className="p-12">
+            <Chart
+              options={options}
+              series={series}
+              type={item.type}
+              width="900"
+            />
+          </div>
+        </CarouselItem>
+      );
+    });
+
     return (
-      <div>
+      <div className="mt-20">
         <VariableDropDown
           callback={this.receiveCallbackVariable.bind(this)}
         />
@@ -110,16 +186,21 @@ class MainChart extends React.Component {
             callback={this.receiveCallbackTo.bind(this)}
           />
         </ButtonGroup>
-        <Chart
-          options={this.state.options}
-          series={this.state.series}
-          type="line"
-          width="900"
-        />
+        <Carousel
+          interval={false}
+          activeIndex={activeIndex}
+          next={this.next}
+          previous={this.previous}
+        >
+          <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+          {slides}
+          <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+          <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+        </Carousel>
       </div>
     )
   }
-};
+}
 
 function mapStateToProps(state) {
   return {
