f609dee2d0b5213b0de59f4e46893fce11476467
Jerem <jeremy1.pidjot@epitech.eu>
2019-09-10 18:38:52 +0200

    add: formatBytes

--- src/components/TopMemoryUsage/TopMemoryUsage.js
+++ src/components/TopMemoryUsage/TopMemoryUsage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import { ChartUtils } from "../../utils/ChartUtils";
 
 class TopMemoryUsage extends React.Component {
   render() {
@@ -7,10 +8,10 @@ class TopMemoryUsage extends React.Component {
     console.log("values", valuesTab);
     return (
       <div className="column-div">
-        <span className="used-average">used average: {<span className="normal-text">{ valuesTab[0] }</span>}</span>
-        <span className="buff-average">buff average: {<span className="normal-text">{valuesTab[1]}</span>}</span>
-        <span className="cach-average">cach average: {<span className="normal-text">{valuesTab[2]}</span>}</span>
-        <span className="free-average">free average: {<span className="normal-text">{valuesTab[3]}</span>}</span>
+        <span className="used-average">used average: {<span className="normal-text">{ChartUtils.formatBytes(valuesTab[0])}</span>}</span>
+        <span className="buff-average">buff average: {<span className="normal-text">{ChartUtils.formatBytes(valuesTab[1])}</span>}</span>
+        <span className="cach-average">cach average: {<span className="normal-text">{ChartUtils.formatBytes(valuesTab[2])}</span>}</span>
+        <span className="free-average">free average: {<span className="normal-text">{ChartUtils.formatBytes(valuesTab[3])}</span>}</span>
       </div>
     );
   }
